2b0a9098aa0cdc0105651a0d47f1819ac7ac5fe4
joaoffnascimento <thecurrentuser@live.com>
2020-09-26 20:40:15 -0300

    feat: init readerController

---
+++ src/main/java/br/edu/ifs/rfid/apirfid/controllers/ReaderController.java
@@ -0,0 +1,10 @@
+package br.edu.ifs.rfid.apirfid.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reader")
+public class ReaderController {
+
+}

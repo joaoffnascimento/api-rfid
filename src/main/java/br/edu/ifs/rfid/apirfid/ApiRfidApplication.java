63c62a13574bd71245c7a490f97c2bc560a5ef75
joaoffnascimento <thecurrentuser@live.com>
2020-09-26 20:59:13 -0300

    fix: skip database configuration from dependency file

--- src/main/java/br/edu/ifs/rfid/apirfid/ApiRfidApplication.java
+++ src/main/java/br/edu/ifs/rfid/apirfid/ApiRfidApplication.java
@@ -2,10 +2,11 @@ package br.edu.ifs.rfid.apirfid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ApiRfidApplication extends SpringBootServletInitializer{
 	
 	@Override
--- src/main/java/br/edu/ifs/rfid/apirfid/controllers/ReaderController.java
+++ src/main/java/br/edu/ifs/rfid/apirfid/controllers/ReaderController.java
@@ -1,5 +1,8 @@
 package br.edu.ifs.rfid.apirfid.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reader")
 public class ReaderController {
 
+	@GetMapping("/")
+	public ResponseEntity<String> helloWorldRest(){
+		return ResponseEntity.status(HttpStatus.OK).body("Olá mundo REST");
+	}
 }

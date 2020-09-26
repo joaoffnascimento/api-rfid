bf4c4b8d31821af1b5627494fdd3b0640a0246e2
joaoffnascimento <thecurrentuser@live.com>
2020-09-26 17:50:29 -0300

    feat: adding inital sources files, starting project

---
+++ src/main/java/br/edu/ifs/rfid/apirfid/ApiRfidApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.rfid.apirfid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRfidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRfidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifs/rfid/apirfid/ApiRfidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.rfid.apirfid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRfidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

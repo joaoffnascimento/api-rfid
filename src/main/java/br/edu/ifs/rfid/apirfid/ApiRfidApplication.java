97f6eee4bd2d4ddd209b702d816e8f98f3149ef2
joaoffnascimento <thecurrentuser@live.com>
2020-09-26 20:31:13 -0300

    chore: execute app from a servlet

--- src/main/java/br/edu/ifs/rfid/apirfid/ApiRfidApplication.java
+++ src/main/java/br/edu/ifs/rfid/apirfid/ApiRfidApplication.java
@@ -2,9 +2,16 @@ package br.edu.ifs.rfid.apirfid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiRfidApplication {
+public class ApiRfidApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(ApiRfidApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRfidApplication.class, args);

e588303564f27ea4322a5ee594d53140effc1347
joaoffnascimento <thecurrentuser@live.com>
2020-09-27 16:04:59 -0300

    feat: create class for constants, add internal server error msg

---
+++ src/main/java/br/edu/ifs/rfid/apirfid/shared/Constants.java
@@ -0,0 +1,10 @@
+package br.edu.ifs.rfid.apirfid.shared;
+
+public class Constants {
+
+	private static final String INTERNAL_SERVER_ERROR_MSG = "Internal Server Error, please contact our support";
+
+	public static String getInternalServerErrorMsg() {
+		return INTERNAL_SERVER_ERROR_MSG;
+	}
+}

150e6dbfb872ec07970967000a7067b3a5a30bc7
joaoffnascimento <thecurrentuser@live.com>
2020-09-27 17:58:16 -0300

    chore: adding constructor to ReaderService

--- src/main/java/br/edu/ifs/rfid/apirfid/service/ReaderService.java
+++ src/main/java/br/edu/ifs/rfid/apirfid/service/ReaderService.java
@@ -3,6 +3,7 @@ package br.edu.ifs.rfid.apirfid.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,15 @@ import br.edu.ifs.rfid.apirfid.shared.Constants;
 @Service
 public class ReaderService implements IReaderService {
 
-	@Autowired
 	private IReaderRepository readerRepository;
+	private ModelMapper mapper;
+	
+	@Autowired
+	public ReaderService(IReaderRepository readerRepository, ModelMapper mapper) {
+		this.readerRepository = readerRepository;
+		this.mapper = mapper;
+	}
+	
 
 	@Override
 	public Reader createReader(ReaderDto request) {

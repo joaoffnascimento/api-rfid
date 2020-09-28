a1b1eb4bdc209fab7524c999f53613bc88ae1953
joaoffnascimento <thecurrentuser@live.com>
2020-09-28 03:17:38 -0300

    feat: add Employee domain

---
+++ src/main/java/br/edu/ifs/rfid/apirfid/domain/Employee.java
@@ -0,0 +1,29 @@
+package br.edu.ifs.rfid.apirfid.domain;
+
+import java.util.Date;
+
+import javax.persistence.Id;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+	@Id
+	private ObjectId id;
+	private int matSiape;
+	private String nome;
+	private String senha;
+	private String profissao;
+	private Date inicioExercicio;
+	private Occupations occupation;
+	private Departament departament;
+}

e0609cbbad7d41e4668b6b6d42ee5e454e1585a4
joaoffnascimento <thecurrentuser@live.com>
2020-10-03 20:34:10 -0300

    fix: Tag domain

--- src/main/java/br/edu/ifs/rfid/apirfid/domain/Tag.java
+++ src/main/java/br/edu/ifs/rfid/apirfid/domain/Tag.java
@@ -1,23 +1,40 @@
 package br.edu.ifs.rfid.apirfid.domain;
 
+import java.util.Date;
+
 import javax.persistence.Id;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document
+@Document(collection = "tag")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Tag {
 
 	@Id
-	private ObjectId id;
+	private String id;
 	private String epc;
 	private String tipo;
-	private ObjectId activeId;
+	@Id
+	private String activeId;
+	
+	private Date createdAt;
+	private Date updatedAt;
+	
+	public Tag createTag(String epc, String tipo) {
+
+		Tag tag = new Tag();
+
+		tag.epc = epc;
+		tag.tipo = tipo;
+		tag.createdAt = new Date();
+		tag.updatedAt = new Date();
+
+		return tag;
+	}
 }

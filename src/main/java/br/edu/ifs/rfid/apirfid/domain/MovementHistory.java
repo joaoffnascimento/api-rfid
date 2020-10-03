4925badb8520028e81ee6682af2711f967afd00a
joaoffnascimento <thecurrentuser@live.com>
2020-10-03 20:36:50 -0300

    chore: update fields on MovementHistory domain

--- src/main/java/br/edu/ifs/rfid/apirfid/domain/MovementHistory.java
+++ src/main/java/br/edu/ifs/rfid/apirfid/domain/MovementHistory.java
@@ -1,10 +1,10 @@
 package br.edu.ifs.rfid.apirfid.domain;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.persistence.Id;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -18,8 +18,31 @@ import lombok.NoArgsConstructor;
 public class MovementHistory {
 
 	@Id
-	private ObjectId id;
+	private String id;
 	private Date dataHoraMovimentacao;
+	private Date createdAt;
+	private Date updatedAt;
 	private int tipoMovimentacao;
-	private ObjectId activeId;
+	private int numPatrimonio;
+	@Id
+	private String activeId;
+
+	public MovementHistory createMovement(int tipoMovimentacao, String activeId, int numPatrimonio) {
+
+		MovementHistory movementHistory = new MovementHistory();
+
+		movementHistory.tipoMovimentacao = tipoMovimentacao;
+		movementHistory.activeId = activeId;
+		movementHistory.dataHoraMovimentacao = new Date();
+		movementHistory.numPatrimonio = numPatrimonio;
+		movementHistory.createdAt = new Date();
+		movementHistory.updatedAt = new Date();
+
+		return movementHistory;
+	}
+
+	public String getDataHoraMovimentacaoEmString() {
+		SimpleDateFormat formatarData = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return formatarData.format(this.dataHoraMovimentacao);
+	}
 }

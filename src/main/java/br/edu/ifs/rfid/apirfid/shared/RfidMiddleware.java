a7eda57057472310ec5d1337c0e1df404361ace4
joaoffnascimento <thecurrentuser@live.com>
2020-10-12 11:30:30 -0300

    refact: RFID methods, better logging, added try catch and fix return

--- src/main/java/br/edu/ifs/rfid/apirfid/shared/RfidMiddleware.java
+++ src/main/java/br/edu/ifs/rfid/apirfid/shared/RfidMiddleware.java
@@ -1,8 +1,5 @@
 package br.edu.ifs.rfid.apirfid.shared;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,16 +19,11 @@ import org.llrp.ltk.generated.messages.ADD_ACCESSSPEC_RESPONSE;
 import org.llrp.ltk.generated.messages.ADD_ROSPEC;
 import org.llrp.ltk.generated.messages.ADD_ROSPEC_RESPONSE;
 import org.llrp.ltk.generated.messages.DELETE_ACCESSSPEC;
-import org.llrp.ltk.generated.messages.DELETE_ACCESSSPEC_RESPONSE;
 import org.llrp.ltk.generated.messages.DELETE_ROSPEC;
-import org.llrp.ltk.generated.messages.DELETE_ROSPEC_RESPONSE;
 import org.llrp.ltk.generated.messages.ENABLE_ACCESSSPEC;
-import org.llrp.ltk.generated.messages.ENABLE_ACCESSSPEC_RESPONSE;
 import org.llrp.ltk.generated.messages.ENABLE_ROSPEC;
-import org.llrp.ltk.generated.messages.ENABLE_ROSPEC_RESPONSE;
 import org.llrp.ltk.generated.messages.RO_ACCESS_REPORT;
 import org.llrp.ltk.generated.messages.START_ROSPEC;
-import org.llrp.ltk.generated.messages.START_ROSPEC_RESPONSE;
 import org.llrp.ltk.generated.parameters.AISpec;
 import org.llrp.ltk.generated.parameters.AISpecStopTrigger;
 import org.llrp.ltk.generated.parameters.AccessCommand;
@@ -88,7 +80,7 @@ public class RfidMiddleware implements LLRPEndpoint {
 	private static final int TIMEOUT_MS = 10000;
 	private static final int ROSPEC_ID = 123;
 
-	public List<String> epcList = new ArrayList<String>();
+	public List<String> epcList = new ArrayList<>();
 
 	int count = 0;
 
@@ -177,202 +169,6 @@ public class RfidMiddleware implements LLRPEndpoint {
 		return roSpec;
 	}
 
-	public void addROSpec() {
-		ADD_ROSPEC_RESPONSE response;
-
-		ROSpec roSpec = buildROSpec();
-
-		try {
-			ADD_ROSPEC roSpecMsg = new ADD_ROSPEC();
-			roSpecMsg.setROSpec(roSpec);
-			response = (ADD_ROSPEC_RESPONSE) reader.transact(roSpecMsg, TIMEOUT_MS);
-
-			logger.info(response.toXMLString());
-
-			StatusCode status = response.getLLRPStatus().getStatusCode();
-			if (status.equals(new StatusCode("M_Success"))) {
-				logger.info("Add ROSpec adicionado com Sucesso.");
-			} else {
-				logger.error("Erro ao adicionar o ROSpec.");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void enableROSpec() {
-		ENABLE_ROSPEC_RESPONSE response;
-
-		ENABLE_ROSPEC enable = new ENABLE_ROSPEC();
-
-		enable.setROSpecID(new UnsignedInteger(ROSPEC_ID));
-
-		try {
-			response = (ENABLE_ROSPEC_RESPONSE) reader.transact(enable, TIMEOUT_MS);
-
-			logger.info(response.toXMLString());
-			logger.info("Enable Rospec habilitado com Sucesso!");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void startROSpec() {
-		START_ROSPEC_RESPONSE response;
-
-		logger.info("Inciando o ROSpec.");
-
-		START_ROSPEC start = new START_ROSPEC();
-		start.setROSpecID(new UnsignedInteger(ROSPEC_ID));
-
-		try {
-			response = (START_ROSPEC_RESPONSE) reader.transact(start, TIMEOUT_MS);
-
-			logger.info(response.toXMLString());
-			logger.info("StartROSpec Iniciado com Sucesso!.");
-			logger.info("Leitor Iniciado!");
-
-		} catch (Exception e) {
-
-			logger.error("Erro ao iniciar o StartROSpec." + "\n");
-			e.printStackTrace();
-		}
-	}
-
-	public void deleteROSpecs() {
-		DELETE_ROSPEC_RESPONSE response;
-
-		logger.info("Deletando todos os ROSpecs.");
-
-		DELETE_ROSPEC del = new DELETE_ROSPEC();
-
-		del.setROSpecID(new UnsignedInteger(0));
-		try {
-			response = (DELETE_ROSPEC_RESPONSE) reader.transact(del, TIMEOUT_MS);
-
-			logger.info(response.toXMLString());
-
-		} catch (Exception e) {
-
-			logger.error("Erro ao deletar os ROSpec.");
-			e.printStackTrace();
-		}
-	}
-
-	public void errorOccured(String s) {
-		logger.error(s);
-	}
-
-	public void connect(String hostname) {
-
-		reader = new LLRPConnector(this, hostname);
-
-		logger.info("IP:");
-		logger.info(((LLRPConnector) reader).getHost());
-
-		logger.info("PORT:");
-		logger.info(Integer.toString(((LLRPConnector) reader).getPort()));
-
-		try {
-			((LLRPConnector) reader).connect();
-
-			logger.info("Conectado com Sucesso!");
-
-		} catch (LLRPConnectionAttemptFailedException e1) {
-			logger.error("Não foi possivel conectar-se ao leitor");
-			e1.printStackTrace();
-		}
-	}
-
-	public void disconnect() {
-		try {
-			((LLRPConnector) reader).disconnect();
-
-			logger.info("Desconectado com Sucesso!");
-		} catch (Exception e2) {
-
-			logger.error("Não foi possível se desconectar do leitor");
-
-			e2.printStackTrace();
-		}
-	}
-
-	public void enableAccessSpec(int accessSpecID) {
-		ENABLE_ACCESSSPEC_RESPONSE response;
-
-		logger.info("Habilitando o AccessSpec.");
-
-		ENABLE_ACCESSSPEC enable = new ENABLE_ACCESSSPEC();
-
-		enable.setAccessSpecID(new UnsignedInteger(accessSpecID));
-
-		try {
-			response = (ENABLE_ACCESSSPEC_RESPONSE) reader.transact(enable, TIMEOUT_MS);
-
-			logger.info(response.toXMLString());
-
-			logger.info("o enableAccessSpec foi habilitado com sucesso.");
-		} catch (Exception e) {
-
-			logger.error("Erro ao habilitar o enableAccessSpec.");
-
-			e.printStackTrace();
-		}
-	}
-
-	public void deleteAccessSpecs() {
-		logger.info("DELETAR ACESSPEC");
-
-		DELETE_ACCESSSPEC_RESPONSE response;
-
-		logger.info("Deletando todos os AccessSpecs.");
-
-		DELETE_ACCESSSPEC del = new DELETE_ACCESSSPEC();
-
-		del.setAccessSpecID(new UnsignedInteger(0));
-
-		try {
-
-			response = (DELETE_ACCESSSPEC_RESPONSE) reader.transact(del, TIMEOUT_MS);
-
-			logger.info(response.toXMLString());
-		} catch (Exception e) {
-
-			logger.error("Erro ao deletar o AccessSpec.");
-
-			e.printStackTrace();
-		}
-	}
-
-	public void addAccessSpec(int accessSpecID) {
-		ADD_ACCESSSPEC_RESPONSE response;
-
-		AccessSpec accessSpec = buildAccessSpec(accessSpecID);
-
-		logger.info("Adicionando o AccessSpec.");
-		try {
-			ADD_ACCESSSPEC accessSpecMsg = new ADD_ACCESSSPEC();
-
-			accessSpecMsg.setAccessSpec(accessSpec);
-			response = (ADD_ACCESSSPEC_RESPONSE) reader.transact(accessSpecMsg, TIMEOUT_MS);
-
-			logger.info(response.toXMLString());
-
-			StatusCode status = response.getLLRPStatus().getStatusCode();
-			if (status.equals(new StatusCode("M_Success"))) {
-				logger.info("Sucesso ao adicionar o AccessSpec.");
-
-			} else {
-				logger.error("Erro ao adicionar o AccessSpec.");
-			}
-		} catch (Exception e) {
-			logger.error("Erro ao adicionar o AccessSpec.");
-
-			e.printStackTrace();
-		}
-	}
-
 	public C1G2Read buildReadOpSpec() {
 
 		C1G2Read opSpec = new C1G2Read();
@@ -392,7 +188,6 @@ public class RfidMiddleware implements LLRPEndpoint {
 	}
 
 	public AccessSpec buildAccessSpec(int accessSpecID) {
-		logger.info("Construindo o AccessSpec.");
 
 		AccessSpec accessSpec = new AccessSpec();
 
@@ -429,13 +224,13 @@ public class RfidMiddleware implements LLRPEndpoint {
 		BitArray_HEX tagData = new BitArray_HEX();
 		targetTag.setTagData(tagData);
 
-		List<C1G2TargetTag> targetTagList = new ArrayList<C1G2TargetTag>();
+		List<C1G2TargetTag> targetTagList = new ArrayList<>();
 		targetTagList.add(targetTag);
 		tagSpec.setC1G2TargetTagList(targetTagList);
 
 		accessCommand.setAirProtocolTagSpec(tagSpec);
 
-		List<AccessCommandOpSpec> opSpecList = new ArrayList<AccessCommandOpSpec>();
+		List<AccessCommandOpSpec> opSpecList = new ArrayList<>();
 
 		opSpecList.add(buildReadOpSpec());
 
@@ -450,48 +245,230 @@ public class RfidMiddleware implements LLRPEndpoint {
 		return accessSpec;
 	}
 
-	public void run(String hostname) {
+	public Boolean addROSpec() {
+		try {
 
-		logger.info("1 - Conectando-se ao Leitor");
-		connect(hostname);
+			ADD_ROSPEC_RESPONSE response;
 
-		logger.info("2 - Deletando Accespecs");
-		deleteAccessSpecs();
+			ROSpec roSpec = buildROSpec();
 
-		logger.info("3 - Deletando ROSpecs");
-		deleteROSpecs();
+			ADD_ROSPEC roSpecMsg = new ADD_ROSPEC();
 
-		logger.info("4 - Adicionando ROSpecs");
-		addROSpec();
+			roSpecMsg.setROSpec(roSpec);
+			response = (ADD_ROSPEC_RESPONSE) reader.transact(roSpecMsg, TIMEOUT_MS);
 
-		logger.info("5 - Adicionando Accespecs");
-		addAccessSpec(READ_ACCESSSPEC_ID);
+			StatusCode status = response.getLLRPStatus().getStatusCode();
 
-		logger.info("6 - Ativando Accespecs");
-		enableAccessSpec(READ_ACCESSSPEC_ID);
+			if (!status.equals(new StatusCode("M_Success"))) {
+				return Boolean.FALSE;
+			}
 
-		logger.info("7 - Ativando ROSPEC");
-		enableROSpec();
+			return Boolean.TRUE;
 
-		logger.info("8 - Iniciando ROSPec");
-		startROSpec();
-
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
 	}
 
-	public void stop() {
-		logger.info("1 - Delete AccessSpecs");
-		deleteAccessSpecs();
+	public Boolean enableROSpec() {
+		try {
 
-		logger.info("2 - Delete ROSPec");
-		deleteROSpecs();
+			ENABLE_ROSPEC enable = new ENABLE_ROSPEC();
 
-		logger.info("3 - Desconectando-se do Leitor");
-		disconnect();
+			enable.setROSpecID(new UnsignedInteger(ROSPEC_ID));
+
+			reader.transact(enable, TIMEOUT_MS);
+
+			return Boolean.TRUE;
+
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
 	}
 
-	@Override
-	public String toString() {
-		return "Middleware_LLRP SistemaMonitoramentodeAtivos";
+	public Boolean startROSpec() {
+
+		try {
+
+			START_ROSPEC start = new START_ROSPEC();
+
+			start.setROSpecID(new UnsignedInteger(ROSPEC_ID));
+
+			reader.transact(start, TIMEOUT_MS);
+
+			logger.info("Reader Started!");
+
+			return Boolean.TRUE;
+
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
+	}
+
+	public Boolean deleteROSpecs() {
+		try {
+
+			DELETE_ROSPEC del = new DELETE_ROSPEC();
+
+			del.setROSpecID(new UnsignedInteger(0));
+
+			reader.transact(del, TIMEOUT_MS);
+
+			return Boolean.TRUE;
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
+	}
+
+	public void errorOccured(String s) {
+		logger.error(s);
+	}
+
+	public Boolean connect(String hostname) {
+
+		try {
+
+			reader = new LLRPConnector(this, hostname);
+
+			((LLRPConnector) reader).connect();
+
+			logger.info("Connected successfully!");
+
+			return Boolean.TRUE;
+
+		} catch (LLRPConnectionAttemptFailedException e) {
+			return Boolean.FALSE;
+		}
+	}
+
+	public Boolean disconnect() {
+		try {
+
+			((LLRPConnector) reader).disconnect();
+
+			logger.info("Successfully Disconnected!");
+
+			return Boolean.TRUE;
+
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
+	}
+
+	public Boolean enableAccessSpec(int accessSpecID) {
+
+		try {
+
+			ENABLE_ACCESSSPEC enable = new ENABLE_ACCESSSPEC();
+
+			enable.setAccessSpecID(new UnsignedInteger(accessSpecID));
+
+			reader.transact(enable, TIMEOUT_MS);
+
+			return Boolean.TRUE;
+
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
+	}
+
+	public Boolean deleteAccessSpecs() {
+
+		try {
+
+			DELETE_ACCESSSPEC del = new DELETE_ACCESSSPEC();
+
+			del.setAccessSpecID(new UnsignedInteger(0));
+
+			reader.transact(del, TIMEOUT_MS);
+
+			return Boolean.TRUE;
+
+		} catch (Exception e) {
+
+			return Boolean.FALSE;
+		}
+	}
+
+	public Boolean addAccessSpec(int accessSpecID) {
+
+		try {
+
+			ADD_ACCESSSPEC_RESPONSE response;
+
+			AccessSpec accessSpec = buildAccessSpec(accessSpecID);
+
+			ADD_ACCESSSPEC accessSpecMsg = new ADD_ACCESSSPEC();
+
+			accessSpecMsg.setAccessSpec(accessSpec);
+
+			response = (ADD_ACCESSSPEC_RESPONSE) reader.transact(accessSpecMsg, TIMEOUT_MS);
+
+			StatusCode status = response.getLLRPStatus().getStatusCode();
+
+			if (status.equals(new StatusCode("M_Success"))) {
+				return Boolean.TRUE;
+			} else {
+				return Boolean.FALSE;
+			}
+
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
+	}
+
+	public Boolean run(String hostname) {
+
+		try {
+
+			logger.info("1 - Connecting to the Reader");
+			connect(hostname);
+
+			logger.info("2 - Deselecting Accespecs");
+			deleteAccessSpecs();
+
+			logger.info("3 - DeleteROSpecs");
+			deleteROSpecs();
+
+			logger.info("4 - Adding ROSpecs");
+			addROSpec();
+
+			logger.info("5 - Adding Accespecs");
+			addAccessSpec(READ_ACCESSSPEC_ID);
+
+			logger.info("6 - Activating Accespecs");
+			enableAccessSpec(READ_ACCESSSPEC_ID);
+
+			logger.info("7 - Activating ROSPEC");
+			enableROSpec();
+
+			logger.info("8 - Starting ROSPec");
+			startROSpec();
+
+			return Boolean.TRUE;
+
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
+	}
+
+	public Boolean stop() {
+		try {
+
+			logger.info("1 - Delete AccessSpecs");
+			deleteAccessSpecs();
+
+			logger.info("2 - Delete ROSPec");
+			deleteROSpecs();
+
+			logger.info("3 - Disconnecting from the Reader");
+			disconnect();
+
+			return Boolean.TRUE;
+
+		} catch (Exception e) {
+			return Boolean.FALSE;
+		}
 	}
 
 	public Boolean inserirNoHistorico(String epc, Boolean calculaTempo) {
@@ -536,13 +513,13 @@ public class RfidMiddleware implements LLRPEndpoint {
 
 				return Boolean.TRUE;
 			}
-			
-			Boolean isToMove = FnUtil.isToMove(calculaTempo, diferencaEmMinutos(movementHistory));
-					
+
+			Boolean isToMove = FnUtil.isToMove(calculaTempo, FnUtil.diferencaEmMinutos(movementHistory));
+
 			if (Boolean.TRUE.equals(isToMove)) {
 
 				logger.info(active.getModelo() + " - It's been five minutes since the last move... Moving");
-				
+
 				if (active.getLastMovimentacao() == 1) {
 
 					activeService.updateMovimentacao(0, active.getId(), active.getNumeroPatrimonio());
@@ -551,7 +528,7 @@ public class RfidMiddleware implements LLRPEndpoint {
 				} else {
 
 					activeService.updateMovimentacao(1, active.getId(), active.getNumeroPatrimonio());
-					
+
 					return Boolean.TRUE;
 				}
 			}
@@ -559,22 +536,10 @@ public class RfidMiddleware implements LLRPEndpoint {
 			logger.info("LAST MOVEMENT: " + active.getLastMovimentacao());
 
 			return Boolean.TRUE;
-			
+
 		} catch (Exception e) {
 			return Boolean.FALSE;
 		}
-	}
-
-	public long diferencaEmMinutos(MovementHistory historico) {
-		LocalDateTime dataHoraAtual = LocalDateTime.now();
-
-		String dataEmString = historico.getDataHoraMovimentacaoEmString();
-
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-		LocalDateTime dataHoraRegistro = LocalDateTime.parse(dataEmString, formatter);
-
-		return ChronoUnit.MINUTES.between(dataHoraRegistro, dataHoraAtual);
 	}
 
 	@Override
@@ -617,5 +582,10 @@ public class RfidMiddleware implements LLRPEndpoint {
 				}
 			}
 		}
+	}
+
+	@Override
+	public String toString() {
+		return "Middleware_LLRP SistemaMonitoramentodeAtivos";
 	}
 }
